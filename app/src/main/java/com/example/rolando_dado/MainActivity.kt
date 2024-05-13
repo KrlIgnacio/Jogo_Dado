04c11cbe1a9192be1dc97d149a62aba5ee06dc6a
KrlIgnacio <karolainesignacio@gmail.com>
2024-05-13 19:05:24 -0300

    app dado

---
+++ app/src/main/java/com/example/rolando_dado/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.example.rolando_dado
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.rolando_dado.ui.theme.RolandodadoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RolandodadoTheme {
+                RolandoDadosApp()
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+
+fun RolandoDadosApp(){
+    DadosComBtnImagem(modifier = Modifier
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center)
+    )
+}
+
+@Composable
+
+fun DadosComBtnImagem(modifier: Modifier = Modifier){
+    var resultado by remember { mutableStateOf(1)}
+    val imageResource = when(resultado){
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(imageResource), contentDescription = resultado.toString())
+        Button(onClick = { resultado = (1..6).random() }) {
+            Text(stringResource(R.string.rolar), fontSize = 24.sp)
+        }
+    }
+
+}
